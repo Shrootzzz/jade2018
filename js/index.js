e04447212f9c8621bb59856217b61f50a252947a
William Wang <William@dyn-160-39-138-162.dyn.columbia.edu>
2018-01-12 12:01:51 -0500

    t fsdjkfnsdkfbdsgkjs

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-$('img').on('mouseover', function() {
-  $(this).animate({
-    "letter-spacing": "10px",
-    "font-size": "40px"
-  }, 1000);
+$("img").hover(function() {
+    $(this).closest(".img").css("z-index", 1);
+
+    $(this)
+        .animate({
+          height: "75px", 
+          width: "75px", 
+          left: "-=55", 
+          top: "-=55"  
+        }, 400, function() {
+            $(this).animate({height: "50px", width: "50px"}, 100);
+    });
+
+$('p').on('click', function() {
+    $(this).toggleClass('bold');
 });
+})
+
