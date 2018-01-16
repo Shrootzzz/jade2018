c83a19305fe08e13c2e8479dfe06434190f90678
William Wang <William@dyn-160-39-138-23.dyn.columbia.edu>
2018-01-16 12:08:12 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,23 +1,36 @@
-$("img").on('mouseover',function() {
+$(document).ready(function() {
+  
+  var windowHeight = $( window ).height();
+  $("div.body-text").css({top: windowHeight});
+  $("div.body-text").css({height: windowHeight});
+
+  $("img.facebook").on('mouseover',function() {
+    // var img = document.getElementsByClassName("facebook");
+    // var width = img.clientWidth * 1.2;
+    // var height = img.clientHeight * 1.2;
     $(this)
         .animate({
-          height: "60px", 
-          width: "60px", 
-          left: "-=55", 
-          top: "-=55"  
+          height: "120%", 
+          width: "120%", 
+          // left: "-=width/2", 
+          // top: "-=height/2"  
         }, 400)
-    });
+  });
 
-$("img").on('mouseout',function() {
-    $(this)
-        .animate({
-          height: "50px", 
-          width: "50px",  
-        }, 400)
-    });
+  $("img.facebook").on('mouseout',function() {
+      $(this)
+          .animate({
+            height: "100%", 
+            width: "100%",  
+          }, 400)
+  });
 
-$('p').on('click', function() {
-    $(this).toggleClass('bold');
+  $('p').on('click', function() {
+      $(this).toggleClass('bold');
+  });
+
 });
+
+
 
 
